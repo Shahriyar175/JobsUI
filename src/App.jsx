fc43a2e1ea82698a4f200bb6202f6176bc177ec8
Shahryar <amirsafavi175@gmail.com>
2026-03-26 22:42:15 +0100

    Added Edit and Delete jobs

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import JobsPage from './pages/JobsPage'
 import NotFound from './pages/NotFound';
 import JobPage, {jobLoader} from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 import axios from 'axios'
 
 // Add New Job
@@ -14,14 +15,26 @@ const addJob = async (newJob) => {
   return;
 }
 
+// Delete Job
+const deleteJob = async (jobId) => {
+  await axios.delete(`/api/jobs/${jobId}`);
+  return;
+}
+
+//Update Job 
+const updateJob = async (job) => {
+  await axios.put(`/api/jobs/${job.id}`, job);
+  return;
+}
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout/>}>
       <Route index element={<HomePage />}/>
       <Route path='/jobs' element={<JobsPage />} />
       <Route path='*' element={<NotFound />} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+      <Route path='/jobs/:id' element={<JobPage deleteJobFunc={deleteJob} />} loader={jobLoader} />
       <Route path='/add-job' element={<AddJobPage addJobfunc={addJob} />} shouldRevalidate={() => false} />
+      <Route path='/edit-job/:id' element={<EditJobPage updateJobFunc={updateJob} />} loader={jobLoader}/>
     </Route>
   )
 );
